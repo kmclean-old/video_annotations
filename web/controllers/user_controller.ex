50abdc40fa602279582cd5bef42634634b7dc92c
Kira McLean <kiraemclean@gmail.com>
2017-03-21 23:54:51 -0400

    create and save users

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -11,5 +11,25 @@ defmodule Rumbl.UserController do
     user = Repo.get(User, id)
     render conn, "show.html", user: user
   end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:alert, "Could not create user")
+        |> render("new.html", changeset: changeset)
+    end
+  end
 end
 
--- web/models/user.ex
+++ web/models/user.ex
@@ -9,4 +9,10 @@ defmodule Rumbl.User do
 
     timestamps()
   end
+
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, [:name, :username])
+    |> validate_length(:username, min: 1, max: 20)
+  end
 end
