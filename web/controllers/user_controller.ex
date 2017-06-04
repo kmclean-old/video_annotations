ec81248fb409b8e93df658d00871f05b7c912bc7
Kira McLean <kiraemclean@gmail.com>
2017-06-03 21:11:34 -0400

    make authenticate user available everywhere

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,6 +1,6 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
   alias Rumbl.User
 
   def index(conn, _params) do
@@ -31,17 +31,6 @@ defmodule Rumbl.UserController do
         conn
         |> put_flash(:alert, "Could not create user")
         |> render("new.html", changeset: changeset)
-    end
-  end
-
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page.")
-      |> redirect(to: page_path(conn, :index))
-      |> halt
     end
   end
 end
