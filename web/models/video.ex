9087751136d01114d37788d8c243f602dfe2e1d6
Kira McLean <kiraemclean@gmail.com>
2017-06-30 15:52:52 -0400

    slugify title for videos

--- web/models/video.ex
+++ web/models/video.ex
@@ -5,6 +5,7 @@ defmodule Rumbl.Video do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
 
@@ -18,6 +19,21 @@ defmodule Rumbl.Video do
     model
     |> cast(params, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
+    |> slugify_title()
     |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(string) do
+    string
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
   end
 end
