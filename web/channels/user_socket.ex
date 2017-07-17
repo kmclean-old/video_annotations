63711fa43d9f3ce298a3d57fc715941ddc920302
Kira McLean <kiraemclean@gmail.com>
2017-07-17 08:03:48 -0400

    authenticate users in channels

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -1,6 +1,8 @@
 defmodule Rumbl.UserSocket do
   use Phoenix.Socket
 
+  @max_age 2 * 7 * 24 * 60 * 60
+
   # Channels
   channel "videos:*", Rumbl.VideoChannel
 
@@ -19,9 +21,17 @@ defmodule Rumbl.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket) do
+    case Phoenix.Token.verify(
+      socket, "user socket", token, max_age: @max_age) do
+      {:ok, user_id} ->
+          {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} ->
+        :error
+    end
   end
+
+  def connect(_params, _socket), do: :error
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
@@ -32,6 +42,5 @@ defmodule Rumbl.UserSocket do
   #
   #     Rumbl.Endpoint.broadcast("users_socket:#{user.id}", "disconnect", %{})
   #
-  # Returning `nil` makes this socket anonymous.
-  def id(_socket), do: nil
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
 end
--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -13,9 +13,9 @@ defmodule Rumbl.Auth do
 
     cond do
       user = conn.assigns[:current_user] ->
-        conn
+        put_current_user(conn, user)
       user = user_id && repo.get(Rumbl.User, user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
       true ->
         assign(conn, :current_user, nil)
     end
@@ -34,7 +34,7 @@ defmodule Rumbl.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
@@ -56,5 +56,13 @@ defmodule Rumbl.Auth do
 
   def logout(conn) do
     configure_session(conn, drop: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 end
