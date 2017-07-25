be705ed4bc4da584069b94220c219d282d0c6603
Kira McLean <kiraemclean@gmail.com>
2017-07-24 23:13:11 -0400

    add info system proxy

---
+++ lib/rumbl/info_system.ex
@@ -0,0 +1,25 @@
+defmodule Rumbl.InfoSystem do
+  @backends [Rumbl.InfoSystem.Wolfram]
+
+  defmodule Result do
+    defstruct score: 0, test: nil, url: nil, backend: nil
+  end
+
+  def start_link(backend, query, query_ref, owner, limit) do
+    backend.start_link(query, query_ref, owner, limit)
+  end
+
+  def compute(query, opts \\ []) do
+    limit = opts[:limit] || 10
+    backends = opts[:backends] || @backends
+
+    backends
+    |> Enum.map(&spawn_query(&1, query, limit))
+  end
+
+  defp spawn_query(backend, query, limit) do
+    query_ref = make_ref()
+    opts = [backend, query, query_ref, self(), limit]
+    {:ok, pid} = Supervisor.start_child(Rumbl.InfoSystem.Supervisor, opts)
+  end
+end
