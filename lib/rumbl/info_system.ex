cd0bfd0a7f8bd3ebf6854a49514d9a4c389dc579
Kira McLean <kiraemclean@gmail.com>
2017-07-25 22:52:42 -0400

    add wolfram info system

--- lib/rumbl/info_system.ex
+++ lib/rumbl/info_system.ex
@@ -2,7 +2,7 @@ defmodule Rumbl.InfoSystem do
   @backends [Rumbl.InfoSystem.Wolfram]
 
   defmodule Result do
-    defstruct score: 0, test: nil, url: nil, backend: nil
+    defstruct score: 0, text: nil, url: nil, backend: nil
   end
 
   def start_link(backend, query, query_ref, owner, limit) do
@@ -15,11 +15,36 @@ defmodule Rumbl.InfoSystem do
 
     backends
     |> Enum.map(&spawn_query(&1, query, limit))
+    |> await_results(opts)
+    |> Enum.sort(&(&1.score >= &2.score))
+    |> Enum.take(limit)
   end
 
   defp spawn_query(backend, query, limit) do
     query_ref = make_ref()
     opts = [backend, query, query_ref, self(), limit]
     {:ok, pid} = Supervisor.start_child(Rumbl.InfoSystem.Supervisor, opts)
+    monitor_ref = Process.monitor(pid)
+    {pid, monitor_ref, query_ref}
+  end
+
+  defp await_results(children, _opts) do
+    await_result(children, [], :infinity)
+  end
+
+  defp await_result([head|tail], acc, timeout) do
+    {pid, monitor_ref, query_ref} = head
+
+    receive do
+      {:results, ^query_ref, results} ->
+        Process.demonitor(monitor_ref, [:flush])
+        await_result(tail, results ++ acc, timeout)
+      {:DOWN, ^monitor_ref, :process, ^pid, _reason} ->
+        await_result(tail, acc, timeout)
+    end
+  end
+
+  defp await_result([], acc, _) do
+    acc
   end
 end
--- mix.exs
+++ mix.exs
@@ -30,15 +30,18 @@ defmodule Rumbl.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 3.0"}]
+    [
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:comeonin, "~> 3.0"},
+      {:sweet_xml, "~> 0.6.5"},
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
